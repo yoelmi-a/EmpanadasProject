1d68474f82841f40bc9a66af1525c74dc07e0543
cyyyyyd <cydchapman@icloud.com>
2026-02-09 20:49:44 -0400

    módulo opciones de pago

---
+++ EmpanadasProject.Data/Services/PaymentService.cs
@@ -0,0 +1,28 @@
+﻿using EmpanadasProject.Data.Models;
+
+namespace EmpanadasProject.Data.Services
+{
+    public class PaymentService
+    {
+        private static readonly List<Payment> payments = new();
+
+        public Payment Create(decimal amount, PaymentMethod method)
+        {
+            var payment = new Payment
+            {
+                Id = payments.Count + 1,
+                Amount = amount,
+                Method = method,
+                IsConfirmed = true
+            };
+
+            payments.Add(payment);
+            return payment;
+        }
+
+        public List<Payment> GetAll()
+        {
+            return payments;
+        }
+    }
+}
