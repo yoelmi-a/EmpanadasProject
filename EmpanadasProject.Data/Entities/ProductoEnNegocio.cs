4d234f9141f9d27abf968541fb816d8067a5bd46
Yoelmi Alcala <alcalayoelmi.a@gmail.com>
2026-03-16 11:08:01 -0400

    Prueba

--- EmpanadasProject.Data/Entities/ProductoEnNegocio.cs
+++ EmpanadasProject.Data/Entities/ProductoEnNegocio.cs
@@ -3,6 +3,7 @@ using System;
 
 namespace EmpanadasProject.Data.Entities
 {
+    //Aqui nada paso
     public class ProductoEnNegocio : ProductoBase
     {
         public int NegocioId { get; set; }
